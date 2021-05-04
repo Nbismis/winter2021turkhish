f413ec59adbbb36d03e2daf762047f7ed55b2395
Nbismis <nazlizehradaglar@gmail.com>
2021-05-04 13:36:53 +0300

    javadeneme

---
+++ src/d_deneme/d1_if.java
@@ -0,0 +1,30 @@
+package d_deneme;
+
+public class d1_if {
+
+	public static void main(String[] args) { 
+		
+		
+		int s1 = 60;
+		int s2 = 75;
+		 
+		if(s1 > s2){
+		System.out.println(s1 + " büyüktür " + s2+ " sayısından");
+		}
+		else if(s1 < s2){
+		System.out.println(s1 + " küçüktür " + s2+ " sayısından");
+		}
+		else{
+		System.out.println(s1 + " eşittir " + s2);
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
